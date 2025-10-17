9e4dae6becd6f840238248a89cd5e277bbd83fd3
Dan Simek <djsimek@yahoo.com>
2025-10-17 13:48:39 -0500

    5.1 Intro to NavigationStack and NavigationLink

---
+++ ToDoList/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  ToDoList
+//
+//  Created by Dan Simek on 10/17/25.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("Welcome to the new view!")
+    }
+}
+
+#Preview {
+    DetailView()
+}
